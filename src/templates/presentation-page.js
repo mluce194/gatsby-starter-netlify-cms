4bf6138b2746f7a791933dd1d3608225a5f86b28
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2020-06-29 11:55:42 +0200

    suppression de l'image de fond

--- src/templates/presentation-page.js
+++ src/templates/presentation-page.js
@@ -13,11 +13,7 @@ export const PresentationPageTemplate = ({image, heading, main1}) => (
 
     <div
       className="full-width-image-container margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-      }}
+    
     >
       <h2
         className="has-text-weight-bold is-size-1"
