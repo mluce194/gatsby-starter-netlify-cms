81249db9dcd40afffcdab07ae21a2e767e5e5ba7
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2020-07-13 14:47:00 +0200

    rintgération d'images dans rubriques honoraires et ventes judiciaires

--- src/templates/VentesJudiciaires-page.js
+++ src/templates/VentesJudiciaires-page.js
@@ -4,11 +4,31 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const VentesJudiciairesPageTemplate = ({ title, content, contentComponent }) => {
+export const VentesJudiciairesPageTemplate = ({ title, image, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <section className="section section--gradient">
+      <div
+        className="full-width-image-container margin-top-0"
+        style={{
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+        }}
+      >
+      <h2
+        className="has-text-weight-bold is-size-1"
+        style={{
+          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+          backgroundColor: '#f40',
+          color: 'white',
+          padding: '1rem',
+        }}
+      >
+        {title}
+      </h2>
+    </div>
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
@@ -39,6 +59,7 @@ const VentesJudiciairesPage = ({ data }) => {
       <VentesJudiciairesPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        image={post.frontmatter.image}
         content={post.html}
       />
     </Layout>
@@ -57,6 +78,13 @@ export const VentesJudiciairesPageQuery = graphql`
       html
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
