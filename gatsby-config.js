e240ec09d5bb53ba0dd5c2a20970055d0d817d73
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2020-08-04 09:29:57 +0200

    correction bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,13 +84,6 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    {
-      resolve: 'gatsby-plugin-i18n',
-      options: {        
-        langKeyDefault: 'fr',
-        useLangKeyLayout: false,
-      }
-    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -80,7 +80,6 @@ export default () => (
               id
               fields {
                 slug
-                langKey
               }
               frontmatter {
                 title
