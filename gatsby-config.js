13be4d5e11c45ada3dd0160c6786ddb021fba25c
Marie-Luce Beraudo <contact@marieluceberaudo.com>
2021-05-07 13:30:11 +0200

    ajout google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,24 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5SZ8JR4",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
